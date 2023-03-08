62267cddc5244f5772924e15ec038012e5c9d151
ntanga87 <ntanga79@gmail.com>
2023-03-08 01:05:23 -0800

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  function that prints a string
+ *
+ * @s: string to receive
+ *
+ * Return: Always 0 (Succes)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
